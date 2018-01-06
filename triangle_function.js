0827ffc008dcd525d8c99bd1a45ceffe7af2d955
Ewa Stosik <e.stosik@outlook.com>
2018-01-06 20:40:39 +0100

    First correction

--- triangle_function.js
+++ triangle_function.js
@@ -1,13 +1,13 @@
 function getTriangleArea(a, h) {
-if (a,h == 0) {
-    console.log('Nieprawidłowe dane');
-}
-else if (a,h <= 0) {
-    console.log('Nieprawidłowe dane');
-} 
-else if (a,h >= 0) {
-    return 'a*h/2'; 
-} 
+	if (a === 0 || h == 0) {
+		console.log('Nieprawidłowe dane');
+	}
+	if (a,h == 0) {
+		console.log('Nieprawidłowe dane');
+	} 
+	else if (a,h <= 0) {
+		return 'a*h/2'; 
+	} 
  
 }
 
